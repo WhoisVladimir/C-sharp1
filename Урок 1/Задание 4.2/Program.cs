b832aa5b66edd4737e0bbdc687b9bdf2560ec7a1
Vladimir Konstantinov <vakonstanti@gmail.com>
2019-11-29 13:14:52 +0300

    Задание 4.2

---
+++ Урок 1/Задание 4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4._2
+/* Написать программу обмена значениями двух переменных: б) *без использования третьей переменной.
+ Константинов В.А.*/
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 2;
+
+            (a, b) = (b, a);
+        }
+    }
+}
