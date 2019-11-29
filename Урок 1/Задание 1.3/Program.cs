7e73db98e31f25c5c640768f241197d841e7d63c
Vladimir Konstantinov <vakonstanti@gmail.com>
2019-11-29 12:48:24 +0300

    Задание 1.3

---
+++ Урок 1/Задание 1.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1._3
+/*Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку: в) используя вывод со знаком $.
+ Константинов В.А.*/
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите Ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите Вашу фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write("Введите Ваш возраст: ");
+            var age = Console.ReadLine();
+            Console.Write("Введите Ваш рост (см): ");
+            var height = Console.ReadLine();
+            Console.Write("Введите Ваш вес (кг): ");
+            var weight = Console.ReadLine();
+
+            Console.WriteLine($"{name} {surname}, в возрасте {age} лет, и при росте {height} см - вполне нормально весить {weight} кг!");
+            Console.ReadKey();
+        }
+    }
+}
