b548bd3d46f421f52eac4d7247181e3f729a5750
Vladimir Konstantinov <vakonstanti@gmail.com>
2019-11-29 13:34:08 +0300

    Задание 5.1

---
+++ Урок 1/Задание 5.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5._1
+/* а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания. 
+ Константинов В.А.*/
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Имя: Владимир. \nФамилия: Константинов. \nГород: Одинцово.");
+            Console.Read();
+        }
+    }
+}
