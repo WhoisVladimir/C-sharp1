97364e646fe1565f0a946a543f2d4df3e1c5bb0a
Vladimir Konstantinov <vakonstanti@gmail.com>
2019-12-06 04:56:26 +0300

    Задание 1а.б.

---
+++ Урок 3/Задание 1.б/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1.б
+  //  б) Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса;
+    // Константинов В.А.
+{
+    public class Complex
+    {
+        double a;
+        double b;
+        public Complex()
+        {
+            a = 0;
+            b = 0;
+        }
+        public Complex(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public Complex Plus(Complex z2)
+        {
+            Complex z3 = new Complex();
+            z3.a = z2.a + a;
+            z3.b = z2.b + b;
+            return z3;
+        }
+        public Complex Minus(Complex z2)
+        {
+            Complex z3 = new Complex();
+            z3.a = a - z2.a;
+            z3.b = b - z2.b;
+            return z3;
+        }
+        public Complex Multiply(Complex z2)
+        {
+            Complex z3 = new Complex();
+            z3.a = a * z2.a - b * z2.b;
+            z3.b = a * z2.b + b * z2.a;
+            return z3;
+        }
+        public string ToString()
+        {
+            return $"{a} + {b}i";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex z1;
+            z1 = new Complex(3, 3);
+            Complex z2 = new Complex(2, 2);
+            Complex Sum = z1.Plus(z2);
+            Complex Subtract = z1.Minus(z2);
+            Complex Increase = z1.Multiply(z2);
+          
+            Console.WriteLine(Sum.ToString());
+            Console.WriteLine(Subtract.ToString());
+            Console.WriteLine(Increase.ToString());
+        }
+    }
+
+   
+}
---
+++ Урок 3/Задание 1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    // а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.
+       
+                // Константинов В.А.
+
+    struct Complex
+    {
+        public double a, b;
+
+        public Complex(double v1, double v2)
+        {
+            a = v1;
+            b = v2;
+        }
+
+        public string Print()
+        {
+            return $"{ a} + { b}i";
+        }
+
+        public Complex Sum(Complex N1, Complex N2)
+        {
+            return new Complex(N1.a + N2.a, N1.b + N2.b);
+        }
+
+        public Complex Subtract(Complex N1, Complex N2)       
+        {
+            return new Complex(N1.a - N2.a, N1.b - N2.b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex z1 = new Complex(1, 2);
+            Complex z2 = new Complex(6, 9);
+            Complex z3 = z1.Sum(z1, z2);
+            Complex z4 = z1.Subtract(z1, z2);
+           
+            Console.WriteLine(z1.Print());
+            Console.WriteLine(z2.Print());
+            Console.WriteLine(z3.Print());
+            Console.WriteLine(z4.Print());
+        }
+    }
+}
